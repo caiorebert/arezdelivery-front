b2afb6faf425e584ce37c38bfa1b71151faab7ea
Caio Rebert <caiorebert070802.cr@gmail.com>
2026-09-15 16:27:57 -0300

    Corrigindo next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,16 @@ const nextConfig = {
         domains: ['upload.wikimedia.org', 'conteudo.imguol.com.br'],
     },
     output: 'standalone',
+    async rewrites() {
+        return [
+            {
+            // Tudo que bater em /api/backend/ no navegador...
+            source: '/api/backend/:path*',
+            // ...será redirecionado internamente para o container NestJS
+            destination: 'http://api:3001/:path*',
+            },
+        ];
+    },
 };
 
 export default nextConfig;
